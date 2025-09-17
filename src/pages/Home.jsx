b87b46a7ee90e8c22548600d95bf6aa67337c96a
hiren <hiren.techvivanta@outlook.com>
2025-09-17 16:33:56 +0530

    fix import in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import HomeBanner from '../components/Home_Components/HomeBanner'
 import Gatherings from '../components/Home_Components/Gatherings'
 import CustomConveyor from '../components/Home_Components/CustomConveyor'
 import ModulerSolution from '../components/Home_Components/ModulerSolution'
-import ProductShowCase from '../components/Home_Components/ProductShowCase'
+import ProductShowCase from '../components/Home_Components/ProductShowcase'
 import Navbar from '../components/Navbar/Navbar'
 
 export default function Home() {
